8295587cb869c32fb2dcd53cb025c972606dea9a
IanMV <ianmatheusv54@gmail.com>
2025-02-05 09:28:22 -0300

    Arrumando IA

--- Backend/Ernestro.js
+++ Backend/Ernestro.js
@@ -31,7 +31,7 @@ function Admensagem(quem, texto) {
                 contadorLetras = 0;
             };
         };
-        mensagem.innerHTML = final.join("");
+        mensagem.textContent = final.join("");
     };
 
     mensagem.className = quem === "ia" ? "mensagemIa" : "mensagemUser";
@@ -39,18 +39,22 @@ function Admensagem(quem, texto) {
     messagesDiv.appendChild(mensagem);
 
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
-}
+};
 
 Admensagem("ia", "Oi! Eu sou o Ernestro, seu mascote virtual. Estou aqui para te ajudar com dicas de exercícios personalizados.🐶");
 Admensagem("ia", "Para começar, qual é o seu exercício favorito ou o tipo de atividade física que você mais gosta?");
 
 async function SendMensagem() {
+    mensagemEspera = false;
+    sendButton.classList.remove('enviar')
+    sendButton.classList.add('semEnviar')
     if (!userInput.value) {
         return;
     };
 
     Admensagem('user', userInput.value);
-    userInput.value = "";
+    let mensagemReal = userInput.value
+    userInput.value = '';
 
     try {
         const response = await fetch("http://localhost:5050/chat", {
@@ -58,64 +62,86 @@ async function SendMensagem() {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ message: userInput.value, userId }),
+            body: JSON.stringify({ message: mensagemReal, userId }),
             mode: 'cors',
         });
-
+    
         const data = await response.json();
         Admensagem("ia", data.response);
+        mensagemEspera = true
+        console.log(mensagemEspera)
+        setTimeout(mensagemNaEspera(), 100)
     } catch (error) {
         Admensagem("ia", "Desculpe, algo deu errado. Tente novamente mais tarde.");
         console.log(error);
+        mensagemEspera = true
+        console.log(mensagemEspera)
+        setTimeout(mensagemNaEspera(), 100)
+    };
+   
+};
+
+function mensagemNaEspera() {
+    if (!(userInput.value == '')) {
+        sendButton.classList.remove('semEnviar')
+        sendButton.classList.add('enviar');
     }
+
 }
 
+let mensagemEspera = true;
 
-
-sendButton.addEventListener('click', SendMensagem);
+sendButton.addEventListener('click', () => {
+    SendMensagem();
+    removeCorBotao();
+});
 
 userInput.addEventListener("keydown", (event) => {
     if (event.key === 'Backspace') {
         setTimeout(removeCorBotao, 2);
     };
     if (event.key === 'Enter') {
-        SendMensagem();
-        sendButton.classList.remove('enviar');
-        sendButton.classList.add('semEnviar');
+        setTimeout(() => {
+            if (mensagemEspera) {
+                SendMensagem();
+                sendButton.classList.remove('enviar');
+                sendButton.classList.add('semEnviar');
+            }
+        }, 100);
     };
 });
 
 userInput.addEventListener("keypress", (event) => {
-    let mensagemEnviara = userInput.value.trim()
-    sendButton.classList.add('clicar');
 
+    let mensagemEnviara = userInput.value.trim();
 
     if (mensagemEnviara == '') {
-
-
-
         mensagemEnviara = (event.key);
         if (!(event.key === 'Enter')) {
+            sendButton.classList.remove('semEnviar')
             sendButton.classList.add('enviar');
             mensagemEnviara = mensagemEnviara.toUpperCase();
             mensagemFormatada = Array.from(mensagemEnviara);
             mensagemFormatada = mensagemFormatada.join("");
             userInput.value = mensagemFormatada;
+            console.log(mensagemEspera)
+            if (!mensagemEspera) {
+                sendButton.classList.remove('enviar');
+                sendButton.classList.add('semEnviar');
+            }
             setTimeout(valor, 1);
-        }
-    };
 
+        };
+    };
 
 });
 
 function valor() {
-    console.log(userInput.value);
     let mensagemFormatada = userInput.value;
     mensagemFormatada = Array.from(mensagemFormatada);
     mensagemFormatada.splice(1, 1);
     mensagemFormatada = mensagemFormatada.join("");
     userInput.value = mensagemFormatada;
-
 };
 
 function removeCorBotao() {
